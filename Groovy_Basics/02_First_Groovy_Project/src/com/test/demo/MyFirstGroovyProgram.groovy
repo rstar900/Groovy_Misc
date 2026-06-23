7b5092c0fe31569286f236b0b3b4ad38974c0aaf
rstar900 <rachitgarg900@gmail.com>
2026-06-23 03:12:40 +0200

    Added 02_First_Groovy_Project

---
+++ Groovy_Basics/02_First_Groovy_Project/src/com/test/demo/MyFirstGroovyProgram.groovy
@@ -0,0 +1,12 @@
+package com.test.demo
+
+class MyFirstGroovyProgram {
+
+	static void main(args) {
+		// Display Hello World...!
+		//println('Hello World...!')
+		
+		// Can also write like this (and even get rid of all classes)
+		println 'Hello World...!'
+	}
+}
